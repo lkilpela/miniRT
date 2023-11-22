607dc78677e90e307ec14c5af072638ef736be92
Lumi Kilpeläinen <lumivo-taivainen@Lumis-Air.home>
2023-11-22 09:25:04 +0200

    updated

--- ft_atoi.c
+++ ft_atoi.c
@@ -18,6 +18,21 @@ static int	ft_isspace(int c)
 		|| c == ' ');
 }
 
+static int	ft_is_overflow(long result, int sign, char c)
+{
+	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (c - '0') > 7))
+	{
+		if (sign == 1)
+			return (-1);
+	}
+	else if (result < INT_MIN / 10 || (result == INT_MIN / 10 && (c - '0') > 8))
+	{
+		if (sign == -1)
+			return (0);
+	}
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
 	long	result;
@@ -33,13 +48,12 @@ int	ft_atoi(const char *str)
 		str++;
 	while (ft_isdigit(*str))
 	{
-		if (result > INT_MAX / 10
-			|| (result == INT_MAX / 10 && (*str - '0') > INT_MAX % 10))
+		if (ft_is_overflow(result, sign, *str))
 		{
 			if (sign == 1)
-				return (-1);// overflow
-			else if (sign == -1 && (*str - '0') > INT_MAX % 10 + 1)
-				return (0);// underflow
+				return (-1);
+			if (sign == -1)
+				return (0);
 		}
 		result = result * 10 + (*str - '0');
 		str++;
