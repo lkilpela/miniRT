4d68895d289c5ac76f1ee9c124d7d2894bb07515
lumikil <kilpelainenlumi@gmail.com>
2023-11-22 11:47:10 +0200

    updated

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:34:41 by lkilpela          #+#    #+#             */
-/*   Updated: 2023/11/18 22:14:34 by lkilpela         ###   ########.fr       */
+/*   Updated: 2023/11/22 11:03:26 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,19 @@ static int	ft_isspace(int c)
 		|| c == ' ');
 }
 
-static int	ft_is_overflow(long result, int sign, char c)
+static int	ft_is_overflow(long result, int sign, int digit)
 {
-	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (c - '0') > 7))
-	{
-		if (sign == 1)
-			return (-1);
-	}
-	else if (result < INT_MIN / 10 || (result == INT_MIN / 10 && (c - '0') > 8))
-	{
-		if (sign == -1)
-			return (0);
-	}
+	if ((sign == 1 && result > (INT_MAX - digit) / 10)
+		|| (sign == -1 && result < (INT_MIN + digit) / 10))
+		return (1);
 	return (0);
 }
 
 int	ft_atoi(const char *str)
 {
-	long	result;
-	int		sign;
+	int	result;
+	int	sign;
+	int	digit;
 
 	result = 0;
 	sign = 1;
@@ -48,15 +42,13 @@ int	ft_atoi(const char *str)
 		str++;
 	while (ft_isdigit(*str))
 	{
-		if (ft_is_overflow(result, sign, *str))
+		digit = *str - '0';
+		if (ft_is_overflow(result, sign, digit))
 		{
-			if (sign == 1)
-				return (-1);
-			if (sign == -1)
-				return (0);
+			return (0);
 		}
-		result = result * 10 + (*str - '0');
+		result = (result * 10) + (digit * sign);
 		str++;
 	}
-	return (sign * result);
+	return (result);
 }
--- test_atoi.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int main ()
-{
-	char str[] = "-2147483648";
-	printf("%d\n", ft_atoi(str));
-	printf("%d\n", atoi(str));
-	return (0);
-}
