58a8e69a6fe15f35b019861a16dad51a9502e3d8
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-07-11 10:42:20 +0000

    chore: Add structs.h and mlx_wrapper.c files

---
+++ include/structs.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 10:28:21 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/07/11 10:39:41 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+# include <MLX42/MLX42.h>
+# include <libft.h>
+# include <math.h>
+#include <fcntl.h>  // open
+#include <unistd.h> // close, read, write
+#include <stdio.h>  // printf, perror
+#include <stdlib.h> // malloc, free, exit
+#include <string.h> // strerror
+
+# define WINDOW_WIDTH 1366
+# define WINDOW_HEIGHT 768
+
+#endif
