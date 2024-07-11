d5367a31605650236d2c136cb937c84b65b77b3a
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-07-11 10:42:35 +0000

    fix norm

--- include/structs.h
+++ include/structs.h
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 10:28:21 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/07/11 10:39:41 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/07/11 10:42:32 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include <MLX42/MLX42.h>
 # include <libft.h>
 # include <math.h>
-#include <fcntl.h>  // open
-#include <unistd.h> // close, read, write
-#include <stdio.h>  // printf, perror
-#include <stdlib.h> // malloc, free, exit
-#include <string.h> // strerror
+# include <fcntl.h>  // open
+# include <unistd.h> // close, read, write
+# include <stdio.h>  // printf, perror
+# include <stdlib.h> // malloc, free, exit
+# include <string.h> // strerror
 
 # define WINDOW_WIDTH 1366
 # define WINDOW_HEIGHT 768
