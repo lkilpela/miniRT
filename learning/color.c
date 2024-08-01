2d17b8046697079082d958e5325dbddac0f16971
{lk} <{lkilpela@student.hive.fi}>
2024-08-01 11:09:42 +0300

    u

--- learning/color.c
+++ learning/color.c
@@ -83,27 +83,27 @@ void test_color()
     t_color c1 = create_color(0.9, 0.6, 0.75);
     t_color c2 = create_color(0.7, 0.1, 0.25);
 
-    t_color expected1 = create_color(1.6, 0.7, 1.0);
-    t_color result1 = add_color(c1, c2);
-    assert(equal_color(result1, expected1));
+    t_color expected1 = c1.create(1.6, 0.7, 1.0);
+    t_color result1 = c1.add(c1, c2);
+    assert(c1.equal(result1, expected1));
     printf("Test add_color passed.\n");
 
-    t_color expected2 = create_color(0.2, 0.5, 0.5);
-    t_color result2 = subtract_color(c1, c2);
-    assert(equal_color(result2, expected2));
+    t_color expected2 = c1.create(0.2, 0.5, 0.5);
+    t_color result2 = c1.subtract(c1, c2);
+    assert(c1.equal(result2, expected2));
     printf("Test subtract_color passed.\n");
 
     t_color c3 = create_color(0.2, 0.3, 0.4);
-    t_color expected3 = create_color(0.4, 0.6, 0.8);
-    t_color result3 = multiply_color_by_scalar(c3, 2);
-    assert(equal_color(result3, expected3));
+    t_color expected3 = c3.create(0.4, 0.6, 0.8);
+    t_color result3 = c3.multiply(c3, 2);
+    assert(c3.equal(result3, expected3));
     printf("Test multiply_color_by_scalar passed.\n");
 
     t_color c4 = create_color(1, 0.2, 0.4);
     t_color c5 = create_color(0.9, 1, 0.1);
-    t_color expected4 = create_color(0.9, 0.2, 0.04);
-    t_color result4 = multiply_colors(c4, c5);
-    assert(equal_color(result4, expected4));
+    t_color expected4 = c4.create(0.9, 0.2, 0.04);
+    t_color result4 = c4.multiply_colors(c4, c5);
+    assert(c4.equal(result4, expected4));
     printf("Test multiply_colors passed.\n");
 }
 
