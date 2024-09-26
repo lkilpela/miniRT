f407e851ec8d3a5d581f2871bd3e42fafe33a69d
lumikil <kilpelainenlumi@gmail.com>
2024-09-26 10:33:31 +0300

    Update transformations.c: change include from structs.h to shapes.h

--- src/matrix/transformations.c
+++ src/matrix/transformations.c
@@ -6,11 +6,11 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 10:09:48 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/09/26 10:16:20 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/09/26 10:33:19 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "structs.h"
+#include "shapes.h"
 
 /* TRANSLATION
 ** Return a 4x4 translation matrix
