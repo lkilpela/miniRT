5e1030daa45b40835790122a80986d0aefdde8c4
lumikil <kilpelainenlumi@gmail.com>
2024-09-26 08:46:54 +0300

    Refactor shapes.h: improve organization, update function pointer definitions, and add project-specific includes

--- include/shapes.h
+++ include/shapes.h
@@ -6,24 +6,28 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 13:54:27 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/09/25 19:35:38 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/09/26 08:46:43 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHAPES_H
 # define SHAPES_H
 
+/* Project-Specific Includes */
 # include "structs.h"
 
+/* Macros for Shapes
 # define SHAPE_AS_PLANE(shape) ((t_plane *)(shape)->object)
 # define SHAPE_AS_SPHERE(shape) ((t_sphere *)(shape)->object)
-# define SHAPE_AS_CYLINDER(shape) ((t_cylinder *)(shape)->object)
+# define SHAPE_AS_CYLINDER(shape) ((t_cylinder *)(shape)->object) */
 
-typedef struct s_intersections (*t_local_intersect_func)
-							(struct s_shape *shape, struct s_ray ray);
-typedef struct s_tuple (*t_local_normal_func)
-							(struct s_shape *shape, struct s_tuple point);
+/* Function Pointers */
+typedef struct s_intersections	(*t_local_intersect_func)(struct s_shape *shape,
+							struct s_ray ray);
+typedef struct s_tuple			(*t_local_normal_func)(struct s_shape *shape,
+							struct s_tuple point);
 
+/* SHAPES STRUCT */
 typedef struct s_sphere
 {
 	t_tuple	center;
@@ -43,7 +47,7 @@ typedef struct s_cylinder
 	bool	closed;
 }				t_cylinder;
 
-typedef struct s_shape 
+typedef struct s_shape
 {
 	int						id;
 	void					*object;
@@ -54,13 +58,13 @@ typedef struct s_shape
 }				t_shape;
 
 /* SHAPES.C */
-t_shape			*shape();
+t_shape			*shape(void);
 void			set_transform_shape(t_shape *shape, t_matrix *m);
 t_intersections	intersect_shape(t_shape *shape, t_ray ray);
 t_tuple			normal_at_shape(t_shape *shape, t_tuple world_point);
 
 /* SPHERE.C */
-t_shape			*sphere();
+t_shape			*sphere(void);
 //t_shape			*sphere_2(t_sphere* sp);
 t_intersections	local_intersect_sphere(t_shape *shape, t_ray r);
 //t_intersections	intersect_transformation(t_shape *s, t_ray r);
@@ -69,15 +73,16 @@ t_tuple			local_normal_at_sphere(t_shape *shape, t_tuple point);
 void			set_sphere_params(t_shape *shape, t_tuple center, float radius);
 
 /* PLANE.C */
-t_shape			*plane();
+t_shape			*plane(void);
 t_intersections	local_intersect_plane(t_shape *shape, t_ray r);
 t_tuple			local_normal_at_plane(t_shape *shape, t_tuple point);
 void			set_transform_shape(t_shape *shape, t_matrix *m);
 
 /* CYLINDER.C */
-t_shape			*cylinder();
+t_shape			*cylinder(void);
 t_intersections	local_intersect_cylinder(t_shape *shape, t_ray r);
 void			intersect_caps(t_shape *shape, t_ray r, t_intersections result);
 bool			check_cap(t_ray r, float t);
 t_tuple			local_normal_at_cylinder(t_shape *shape, t_tuple point);
+
 #endif
