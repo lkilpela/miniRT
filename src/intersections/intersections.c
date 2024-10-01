bc985a9c08c7b9184b46d560310d526b06f585da
lumikil <kilpelainenlumi@gmail.com>
2024-10-01 08:36:02 +0300

    Refactor intersection and ray functions: improve code readability by aligning variable declarations and simplifying return statements; enhance memory allocation error handling in intersections_array function.

--- src/intersections/intersections.c
+++ src/intersections/intersections.c
@@ -1,14 +1,13 @@
 #include "structs.h"
-#include "shapes.h"
 
 // Create an intersection object with a given t value and the intersected object
-t_intersection intersection(float t, t_shape *shape)
+t_intersection	intersection(float t, t_shape *shape)
 {
-    t_intersection i;
+	t_intersection	i;
 
-    i.t = t;
-    i.object = shape;
-    return (i);
+	i.t = t;
+	i.object = shape;
+	return (i);
 }
 
 
@@ -18,71 +17,78 @@ t_intersection intersection(float t, t_shape *shape)
 ** - Initialization: Initializes the array with either the provided intersections or default values.
 ** - Count Setting: Sets the count of intersections in xs.
 */
-t_intersections intersections_array(int count, t_intersection *array)
+t_intersections	intersections_array(int count, t_intersection *array)
 {
-    t_intersections xs;
-    xs.count = count;
-    xs.array = calloc(count, sizeof(t_intersection));
-    if (xs.array == NULL)
-        return xs;
+	t_intersections	xs;
+	int				i;
 
-    if (array != NULL)
-    {
-        for (int i = 0; i < count; i++)
-        xs.array[i] = array[i];
-    }
-    else
-    {
-        for (int i = 0; i < count; i++)
-        xs.array[i] = intersection(0, NULL);
-    }
-    return xs;
+	if (array != NULL)
+	{
+		xs.count = count;
+		xs.array = calloc(count, sizeof(t_intersection));
+		if (xs.array == NULL)
+			fatal_error("Failed to allocate memory for intersections_array\n");
+		i = 0;
+		while (i < count)
+		{
+			xs.array[i] = array[i];
+			i++;
+		}
+	}
+	else
+	{
+		xs.count = 0;
+		xs.array = NULL;
+	}
+	return (xs);
 }
 
 /* HIT 
 ** Loop through all intersections and return the intersection with the lowest positive t value
 ** If there are no positive t values, return NULL
 */
-t_intersection *hit(t_intersections *intersections)
+t_intersection	*hit(t_intersections *intersections)
 {
-    t_intersection *hit = NULL;
-    for (int i = 0; i < intersections->count; i++)
-    {
-        if (intersections->array[i].t >= 0)
-        {
-            if (hit == NULL || intersections->array[i].t < hit->t)
-            {
-                hit = &intersections->array[i];
-            }
-        }
-    }
-    return hit;
+	t_intersection	*hit;
+
+	hit = NULL;
+	for (int i = 0; i < intersections->count; i++)
+	{
+		if (intersections->array[i].t >= 0)
+		{
+			if (hit == NULL || intersections->array[i].t < hit->t)
+			{
+				hit = &intersections->array[i];
+			}
+		}
+	}
+	return (hit);
 }
 
+/* PREPARE COMPUTATIONS
+** - Purpose: the computations for shading
+** 1. Copy the intersection's properties, for convenience
+** 2. Precompute some useful values
+** 3. If the dot product is negative, the normal points inwards
+** 4. Compute over_point to avoid shadow acne
+**   - EPSILON is a small number, 1e-6. Is used here to bump the point just a bit in the direction of the normal.
+*/
 t_computations prepare_computations(t_intersection i, t_ray r)
 {
-    t_computations comps;
+	t_computations	comps;
 
-    // Copy the intersection's properties, for convenience
-    comps.t = i.t;
-    comps.shape = i.object;
-
-    // Precompute some useful values
-    comps.point = position(r, comps.t);
-    comps.eyev = negate(r.direction);
-    comps.normalv = normal_at_shape(comps.shape, comps.point);
-    
-
-    // If the dot product is negative, the normal points inwards
-    if (dot(comps.normalv, comps.eyev) < 0) {
-        comps.inside = true;
-        comps.normalv = negate(comps.normalv);
-    } else {
-        comps.inside = false;
-    }
-
-    // Compute over_point to avoid shadow acne
-    // EPSILON is a small number, 1e-6. Is used here to bump the point just a bit in the direction of the normal.
-    comps.over_point = add(comps.point, multiply_by_epsilon(comps.normalv));
-    return comps;
+	comps.t = i.t;
+	comps.shape = i.object;
+	comps.point = position(r, comps.t);
+	comps.eyev = negate(r.direction);
+	comps.normalv = normal_at_shape(comps.shape, comps.point);
+	if (dot(comps.normalv, comps.eyev) < 0)
+	{
+		comps.inside = true;
+		comps.normalv = negate(comps.normalv);
+	}
+	else
+		comps.inside = false;
+	comps.over_point = add(comps.point, multiply_by_epsilon(comps.normalv));
+	return (comps);
 }
--- src/intersections/ray.c
+++ src/intersections/ray.c
@@ -1,33 +1,31 @@
 #include "structs.h"
 
-t_ray ray(t_tuple origin, t_tuple direction)
+t_ray	ray(t_tuple origin, t_tuple direction)
 {
-    t_ray r;
+	t_ray	r;
 
-    r.origin = origin;
-    r.direction = direction;
-    return (r);
+	r.origin = origin;
+	r.direction = direction;
+	return (r);
 }
 
-t_tuple position(t_ray r, float t)
+t_tuple	position(t_ray r, float t)
 {
-    //printf("Ray: Origin: %f %f %f, Direction: %f %f %f\n", r.origin.x, r.origin.y, r.origin.z, r.direction.x, r.direction.y, r.direction.z);
-    //printf("T: %f\n", t);
-    t_tuple result = add(r.origin, multiply_by_scalar(r.direction, t));
-    //printf("result: %f %f %f\n", result.x, result.y, result.z);
-    return result;
+	t_tuple	result;
+
+	result = add(r.origin, multiply_by_scalar(r.direction, t));
+	return (result);
 }
 
 // Translating a ray
-t_ray transform(t_ray r, t_matrix *m)
+t_ray	transform(t_ray r, t_matrix *m)
 {
-    t_ray transformed_ray;
+	t_ray	transformed_ray;
+	t_tuple	direction;
 
-    transformed_ray.origin = matrix_multiply_tuple(m, r.origin);
-
-    t_tuple direction = r.direction;
-    direction.w = 0;
-
-    transformed_ray.direction = matrix_multiply_tuple(m, r.direction);
-    return transformed_ray;
+	transformed_ray.origin = matrix_multiply_tuple(m, r.origin);
+	direction = r.direction;
+	direction.w = 0;
+	transformed_ray.direction = matrix_multiply_tuple(m, r.direction);
+	return (transformed_ray);
 }
