441ea71f39b9b142c1ca5d8d8304cb05629e27af
lumikil <kilpelainenlumi@gmail.com>
2024-10-02 13:30:38 +0300

    Remove unused normal parameter in plane_transform function

--- src/shapes/plane.c
+++ src/shapes/plane.c
@@ -8,6 +8,8 @@ void	plane_transform(t_shape *sp, t_tuple p, t_tuple normal)
 	t_matrix	*scaling_matrix;
 	t_matrix	*rotation_matrix;
 
+	(void)normal;
+
 	rotation_matrix = combine_rotations(0, 0, 0);
 	scaling_matrix = scaling(1, 1, 1);
 	translation_matrix = translation(p.x, p.y, p.z);
