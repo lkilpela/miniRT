ff4cd9136d4ed94f9c939f495516e11128373d32
lumikil <kilpelainenlumi@gmail.com>
2024-10-07 14:01:05 +0300

    Refactor main.c: Clean up code and improve readability

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 14:00:57 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/10/07 14:00:59 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "structs.h"
 
 void	check_args(int argc, char **argv)
@@ -9,31 +21,19 @@ void	check_args(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-    t_world *w; 
+	t_world	*w; 
 
-    check_args(argc, argv);
-    // Create the scene
-    w = create_scene(argv[1]);
-    if (!w)
-        fatal_error("Failed to create scene");
-
-    // Set the key hook
-    mlx_key_hook(w->window.mlx, &key_hook, w->window.mlx);
-
-    // Render the scene
-    render(w->window.img, w);
-
-    // Display the image
+	check_args(argc, argv);
+	w = create_scene(argv[1]);
+	if (!w)
+		fatal_error("Failed to create scene");
+	mlx_key_hook(w->window.mlx, &key_hook, w->window.mlx);
+	render(w->window.img, w);
 	if (mlx_image_to_window(w->window.mlx, w->window.img, 0, 0) < 0)
 		libmlx_error("Failed to put image to window", w->window.mlx);
-
-    // Loop to keep the window open
-    mlx_loop(w->window.mlx);
-
-    // Cleanup
-    mlx_delete_image(w->window.mlx, w->window.img);
-    mlx_terminate(w->window.mlx);
-
-    destroy_world(w);
-    return (EXIT_SUCCESS);
+	mlx_loop(w->window.mlx);
+	mlx_delete_image(w->window.mlx, w->window.img);
+	mlx_terminate(w->window.mlx);
+	destroy_world(w);
+	return (EXIT_SUCCESS);
 }
