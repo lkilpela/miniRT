256d791036d094029acd7f93bcc89a26202e7692
lkilpela <lkilpela@student.hive.fi>
2024-10-09 18:14:07 +0300

    Update ft_realloc to support dynamic resizing of any data type

--- lib/libft/src/string/ft_realloc.c
+++ lib/libft/src/string/ft_realloc.c
@@ -6,29 +6,29 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:46:16 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/10/07 21:51:52 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:58:44 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_realloc(char *origin_array, int origin_count)
+void	*ft_realloc(void *origin_array, int origin_count, size_t type_size)
 {
-	char	*resized_array;
+	void	*resized_array;
 
 	if (origin_count == 0)
 	{
-		resized_array = malloc(sizeof(char) * 2);
+		resized_array = malloc(type_size * 2);
 		if (!resized_array)
 			return (NULL);
 		return (resized_array);
 	}
-	resized_array = malloc(sizeof(char) * (origin_count * 2));
+	resized_array = malloc(type_size * (origin_count * 2));
 	if (!resized_array)
 		return (NULL);
 	if (origin_array)
 	{
-		ft_memcpy(resized_array, origin_array, sizeof(char) * origin_count);
+		ft_memcpy(resized_array, origin_array, type_size * origin_count);
 		free(origin_array);
 	}
 	return (resized_array);
